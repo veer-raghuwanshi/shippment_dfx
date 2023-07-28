564102b5183c287e7caab4dd6baf147a6a4267b6
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-28 17:41:54 +0530

    Update Payment.js

--- routes/Payment.js
+++ routes/Payment.js
@@ -1,8 +1,8 @@
 import express from "express";
 const Router = express.Router();
 import mysql from 'mysql'
-​
-​
+
+
 // MySQL database configuration
 const pool = mysql.createPool({
     host: '89.117.27.154', // Replace with your host name
@@ -11,17 +11,16 @@ const pool = mysql.createPool({
     password: 'pFuSG;@F+8',      // Replace with your database password    ///df-opcity-home
     database: 'u219507986_shipingnew'
  });
-​
-​
+
 // API endpoint for inserting data into the shipments table
 Router.post('/payment', (req, res) => {
   const { full_name, shipment_id, amount, DateAndTime } = req.body;
-​
+
   // Validate the incoming data
   if (!full_name || !shipment_id || !amount) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
-​
+
   // Insert data into the shipments table
   const query = 'INSERT INTO money (full_name, shipment_id, amount, DateAndTime) VALUES (?, ?, ?, ?)';
   pool.query(query, [full_name, shipment_id, amount, DateAndTime], (err, result) => {
@@ -29,11 +28,11 @@ Router.post('/payment', (req, res) => {
       console.error('Error inserting data:', err);
       return res.status(500).json({ error: 'Failed to insert data.' });
     }
-​
+
     console.log('Data inserted successfully.');
     res.status(200).json({ message: 'Data inserted successfully.' });
   });
 });
-​
-​
+
+
 export default Router;
