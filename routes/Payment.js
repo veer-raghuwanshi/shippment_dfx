7eede3ea685681ba5838cb5dea526e75d6528600
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-29 12:05:29 +0530

    updated

--- routes/Payment.js
+++ routes/Payment.js
@@ -4,8 +4,8 @@ const Router = express.Router();
 import mysql from 'mysql'
 import md5 from 'md5'
 import nodemailer from "nodemailer";
-​
-​
+
+
 // MySQL database configuration
 const pool = mysql.createPool({
     host: '89.117.27.154', // Replace with your host name
@@ -14,17 +14,17 @@ const pool = mysql.createPool({
     password: 'pFuSG;@F+8',      // Replace with your database password    ///df-opcity-home
     database: 'u219507986_shipingnew'
  });
-​
-​
+
+
 // API endpoint for inserting data into the shipments table
 Router.post('/payment', (req, res) => {
   const { full_name, shipment_id, amount, DateAndTime } = req.body;
-​
+
   // Validate the incoming data
   if (!full_name || !shipment_id || !amount) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
-​
+
   // Insert data into the shipments table
   const query = 'INSERT INTO money (full_name, shipment_id, amount, DateAndTime) VALUES (?, ?, ?, ?)';
   pool.query(query, [full_name, shipment_id, amount, DateAndTime], (err, result) => {
@@ -32,23 +32,23 @@ Router.post('/payment', (req, res) => {
       console.error('Error inserting data:', err);
       return res.status(500).json({ error: 'Failed to insert data.' });
     }
-​
+
     console.log('Data inserted successfully.');
     res.status(200).json({ message: 'Data inserted successfully.' });
   });
 });
-​
-​
+
+
 // API endpoint to get all data from the "money" table
 Router.get('/getpayment', (req, res) => {
   const query = 'SELECT full_name, shipment_id, amount, DateAndTime FROM money';
-​
+
   pool.query(query, (err, results) => {
     if (err) {
       console.error('Error fetching data:', err);
       return res.status(500).json({ error: 'Failed to fetch data.' });
     }
-​
+
     // const currentDate = new Date().toLocaleString('en-IN', {
     //     timeZone: 'Asia/Kolkata',
     //     hour12: true,
@@ -61,11 +61,11 @@ Router.get('/getpayment', (req, res) => {
       DateAndTime: row.DateAndTime
     //   DateAndTime: row.DateAndTime.toISOString().slice(0, 19).replace('T', ' '), // Format date to 'YYYY-MM-DD HH:mm:ss'
     }));
-​
+
     res.status(200).json(formattedResults);
   });
 });
-​
-​
-​
+
+
+
 export default Router;
